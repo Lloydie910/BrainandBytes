2464c03923e13ae42098ed05fe8da681c481c5af
Lloydie910 <128500322+Lloydie910@users.noreply.github.com>
2023-03-21 15:11:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-document.getElementById("submit-btn").addEventListener("click", function () {
-    // Add your JavaScript code here to calculate the score and display the results
-});
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADHD Questionnaire</title>
+</head>
+<body>
+    <h1>ADHD Questionnaire</h1>
+    <form id="adhd-questionnaire">
+        <p>
+            1. How often do you have trouble wrapping up the final details of a project, once the challenging parts have been done?<br>
+            <select name="q1">
+                <option value="0">Never</option>
+                <option value="1">Rarely</option>
+                <option value="2">Sometimes</option>
+                <option value="3">Very Often</option>
+            </select>
+        </p>
+        <p>
+            2. How often do you have difficulty getting things in order when you have to do a task that requires organization?<br>
+            <select name="q2">
+                <option value="0">Never</option>
+                <option value="1">Rarely</option>
+                <option value="2">Sometimes</option>
+                <option value="3">Very Often</option>
+            </select>
+        </p>
+        <p>
+            3. How often do you have problems remembering appointments or obligations?<br>
+            <select name="q3">
+                <option value="0">Never</option>
+                <option value="1">Rarely</option>
+                <option value="2">Sometimes</option>
+                <option value="3">Very Often</option>
+            </select>
+        </p>
+    </form>
+</body>
+</html>
